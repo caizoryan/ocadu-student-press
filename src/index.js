4d6971f97f60e4b1e75883411bfa43f38989d370
Aaryan Pashine <pashineaaryan@gmail.com>
2022-08-28 17:45:10 -0400

    added landing page type

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Landing from "./landing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Landing />);
---
+++ src/landing.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Landing() {
+  return (
+    <Wrapper>
+      <HeaderContainer>
+        <Header>
+          VOICE <br></br>
+          <OfSpan></OfSpan> OF <br></br> <StudentSpan></StudentSpan> STUDENTS
+        </Header>
+      </HeaderContainer>
+    </Wrapper>
+  );
+}
+
+let secondaryColor = "#FF5252";
+let primaryColor = "#1E1E1E";
+const OfSpan = styled.span`
+  padding-left: 8vw;
+`;
+const StudentSpan = styled.span`
+  padding-left: 3.7vw;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${primaryColor};
+  background-color: ${secondaryColor};
+  width: 100vw;
+  height: 100vh;
+`;
+const HeaderContainer = styled.div`
+  display: flex;
+`;
+const Header = styled.h1`
+  /* padding-top: 20vh;
+  padding-left: 30vw; */
+  font-size: 11vw;
+  line-height: 0.9em;
+  font-family: urw-din-condensed, sans-serif;
+  font-weight: 700;
+`;
